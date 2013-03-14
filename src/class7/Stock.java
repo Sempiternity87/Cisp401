d71f2f7d2b4104b945385734bf56b9129cb2fd9b
Sempiternity87 <Sempiternity87@gmail.com>
2013-03-14 08:02:46 -0700

    Created classes for Stock

---
+++ src/class7/Stock.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+* Stock.java
+*===============================================================================
+* Follow the example of the Circle class we say last week. Create a Stock class,
+* which contains:
+* 
+* - A string variable named symbol containing the stock's symbol.
+* - A string variable called name containing the stock's name.
+* - A double variable called previousClosingPrice the stores the price for the
+*   previous day.
+* - Another double variable called currentPrice containing the current stock price.
+* - A constructor that creates a stock with the specified symbol and name.
+* - A method named getChangePercent that returns the percentage difference between
+*   the previous and current price.
+* - Any other variables and methods you deem necessary.
+* 
+* Draw a UML diagram and then implement the class. Write a test program that
+* creates a Stock object with the symbol ORCL, name Oracle Cororation, previous
+* close of 34.5 and current price of 34.35. Display the price change as a
+* percentage.
+* 
+* I have included my test program, you can modify it as you like. Note: it has
+* useful test data. Please ignore my comments - I know they are terrible, do a
+* better job than I did.
+* 
+* Turn in your Stock class and your test program.
+*===============================================================================
+* @author Chadd Klaus
+* @date   Mar 14, 2013
+*******************************************************************************/
+package class7;
+
+public class Stock
+  {
+  
+  public void setPreviousClosePrice(double price) { }
+  public void setCurrentPrice(double price) {}
+  
+  public Stock(String symbol, String name)
+    {
+    
+    }
+  
+  public void printStock() {}
+  }
